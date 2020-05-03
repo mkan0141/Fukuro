c0f1105a503f6261a43f79f23ba6b821f32b582d
mkan0141 <kanzume0141@gmail.com>
2020-05-04 00:37:31 +0900

    Add: Footer Component

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="flex justify-between bg-gray-800 py-8">
+    <span class="text-white ml-20">&copy;mkan_0141</span>
+    <i class="fab fa-github text-white text-xl mr-20"></i>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
