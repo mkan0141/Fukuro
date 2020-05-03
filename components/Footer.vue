dc5def6fba94333773be87be7f8796500d3a17b0
mkan0141 <kanzume0141@gmail.com>
2020-05-04 00:42:26 +0900

    Update: OctCatをクリックするとFukuroのリポジトリに飛ぶようにした

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="flex justify-between bg-gray-800 py-8">
     <span class="text-white ml-20">&copy;mkan_0141</span>
-    <i class="fab fa-github text-white text-xl mr-20"></i>
+    <a href="https://github.com/mkan0141/Fukuro" class="mr-20"><i class="fab fa-github text-white text-xl"></i></a>
   </footer>
 </template>
 
