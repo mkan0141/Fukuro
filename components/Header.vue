6372075f6d9a29566930ff2687a0c6870a1169ca
mkan0141 <kanzume0141@gmail.com>
2020-05-04 17:09:36 +0900

    Update: usageページへのリンクを貼った

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,26 @@
-<template functional>
+<template>
 <div class="w-full border-b-2 border-gray-800">
   <Alert />
-  <header class="md:max-w-6xl md:m-auto">
-    <nuxt-link to="/">
-      <img src="~/assets/svg/fukuro.svg" class="logo ml-5 md:ml-20">
+  <header class="flex justify-between md:max-w-6xl md:m-auto">
+    <nuxt-link class="ml-3" to="/">
+      <img src="~/assets/svg/fukuro.svg" class="logo">
+    </nuxt-link>
+    <nuxt-link class="flex items-center text-gray-800 font-bold text-xl mr-3" to="/usage">
+      使い方
     </nuxt-link>
   </header>
 </div>
 </template>
+
+<script>
+import Alert from '~/components/Alert'
+
+export default {
+  components: {
+    Alert,
+  }
+}
+</script>
 
 <style scoped>
 .logo {
