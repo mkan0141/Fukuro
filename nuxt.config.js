2fb26a4e2225d883fb23bdf459a85256c58f6775
mkan0141 <kanzume0141@gmail.com>
2020-05-04 20:32:34 +0900

    Update: OGPの設定を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,30 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'og:type', property: 'og:type', content: '' },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Fukuro'
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://fukuro.mkan0141.dev'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: 'Fukuroはブラウザ上で画面を録画できるWebアプリです。'
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://fukuro.mkan0141.dev/ogp/fukuro_logo.png'
+        },
+        { hid: 'og:site_name', name: 'og:site_name', content: 'Fukuro' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
