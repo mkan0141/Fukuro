1c19ae3b7746e243faaa400653019dbefe0ac75d
mkan0141 <kanzume0141@gmail.com>
2020-05-04 13:36:31 +0900

    Update: スタイルを調整した

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,10 @@
 <template functional>
-<div>
+<div class="w-full border-b-2 border-gray-800">
   <Alert />
-  <header class="border-b-2 border-gray-800">
-    <img src="~/assets/svg/fukuro.svg" class="logo ml-20">
+  <header class="md:max-w-6xl md:m-auto">
+    <nuxt-link to="/">
+      <img src="~/assets/svg/fukuro.svg" class="logo ml-5 md:ml-20">
+    </nuxt-link>
   </header>
 </div>
 </template>
