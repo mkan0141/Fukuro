3c4648433ab3fda649cc8bcd5a6db70c3025f42a
mkan0141 <kanzume0141@gmail.com>
2020-05-04 11:10:37 +0900

    Update: Headerの上にAlertを表示するようにした

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,10 @@
 <template functional>
-  <header class="ml-20">
-    <img src="~/assets/svg/fukuro.svg" class="logo">
+<div>
+  <Alert />
+  <header class="border-b-2 border-gray-800">
+    <img src="~/assets/svg/fukuro.svg" class="logo ml-20">
   </header>
+</div>
 </template>
 
 <style scoped>
